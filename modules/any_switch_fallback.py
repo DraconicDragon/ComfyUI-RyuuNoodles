1fab1d0d8d8e351d39f35b8854a1397a8608741a
Drac <altdragon@outlook.de>
2025-04-16 16:54:28 +0200

    refactor: make a BaseSwitchFallback class to easily add more switches with same logic but type specific like ("IMAGE",) or ("LATENT",), AnySwitchFallback now inherits from BaseSwitchFallback

--- modules/any_switch_fallback.py
+++ modules/any_switch_fallback.py
@@ -1,15 +1,25 @@
 from ..common import AnyType, ryuu_print
 
-any_type = AnyType("*")
 
+class BaseSwitchFallback:
+    """Base class for fallback switch nodes."""
 
-class AnySwitchFallback:
-    """RyuuNoodles Any Switch Fallback Node.
-    This node has a boolean option and two inputs that can by any type.
-    If the boolean is True, it returns input_1.
-    If the boolean is False, it returns input_2_opt.
-    If input_2_opt is not provided, it falls back to input_1 and a message will be printed to console.
-    """
+    TYPE = AnyType("*")  # Override in subclasses if needed
+    INPUT_NAME_1 = "input_1"
+    INPUT_NAME_2 = "input_2_opt"
+    TOOLTIP_BOOL = (
+        "True will output {input_1}, False will try to output {input_2_opt}. "
+        "If {input_2_opt} is not provided (e.g. the connected node is muted or bypassed), it will fall back to {input_1}. "
+        "A message will be printed to console if this happens."
+    )
+    TOOLTIP_1 = "The input to output if boolean is True. Will be used as fallback if {input_2_opt} is not provided."
+    TOOLTIP_2 = (
+        "The input to output if boolean is False. If not provided (e.g. the connected node is muted or bypassed), "
+        "{input_1} will be used as fallback and a message will be printed to console if this happens. This input is optional."
+    )
+
+    CATEGORY = "RyuuNoodles/Util/Switches"
+    OUTPUT_NODE = True
 
     @classmethod
     def INPUT_TYPES(cls):
@@ -19,40 +29,59 @@ class AnySwitchFallback:
                     "BOOLEAN",
                     {
                         "default": False,
-                        "tooltip": "True will output input_1, False will try to output input_2_opt. "
-                        + "If input_2_opt is not provided (if for example the connected node is muted or bypassed), it will fall back to input_1. "
-                        + "A message will be printed to console if this happens.",
+                        "tooltip": cls.TOOLTIP_BOOL.format(
+                            input_1=cls.INPUT_NAME_1,
+                            input_2_opt=cls.INPUT_NAME_2,
+                        ),
                     },
                 ),
-                "input_1": (
-                    any_type,
+                cls.INPUT_NAME_1: (
+                    cls.TYPE,
                     {
-                        "tooltip": "The input to output if boolean is True. Will be used as fallback if input_2_opt is not provided",
+                        "tooltip": cls.TOOLTIP_1.format(
+                            input_1=cls.INPUT_NAME_1,
+                            input_2_opt=cls.INPUT_NAME_2,
+                        ),
                     },
                 ),
             },
             "optional": {
-                "input_2_opt": (
-                    any_type,
+                cls.INPUT_NAME_2: (
+                    cls.TYPE,
                     {
-                        "tooltip": "The input to output if boolean is False. If not provided, input_1 will be used as fallback."
+                        "tooltip": cls.TOOLTIP_2.format(
+                            input_1=cls.INPUT_NAME_1,
+                            input_2_opt=cls.INPUT_NAME_2,
+                        ),
                     },
-                ),
+                )
             },
         }
 
-    RETURN_TYPES = (any_type,)
+    RETURN_TYPES = (TYPE,)
     FUNCTION = "do_thing"
-    OUTPUT_NODE = True
 
-    CATEGORY = "RyuuNoodles/Util"
-
-    def do_thing(self, boolean, input_1, input_2_opt=None):
+    def do_thing(self, boolean, **kwargs):
+        input_1 = kwargs.get(self.INPUT_NAME_1)
+        input_2_opt = kwargs.get(self.INPUT_NAME_2)
         if boolean:
             return (input_1,)
+        elif input_2_opt is not None:
+            return (input_2_opt,)
         else:
-            if input_2_opt is not None:
-                return (input_2_opt,)
-            else:
-                ryuu_print("No input_2_opt provided, falling back to input_1")
-                return (input_1,)
+            ryuu_print(f"No {self.INPUT_NAME_2} provided, falling back to {self.INPUT_NAME_1}")
+            return (input_1,)
+
+
+class AnySwitchFallback(BaseSwitchFallback):
+    """RyuuNoodles Any Switch Fallback Node.
+
+    This node has a boolean option and two inputs that can be any type.
+    If the boolean is True, it returns input_1.
+    If the boolean is False, it returns input_2_opt.
+    If input_2_opt is not provided, it falls back to input_1 and a message will be printed to console.
+    """
+
+    TYPE = AnyType("*")
+    INPUT_NAME_1 = "input_1"
+    INPUT_NAME_2 = "input_2_opt"

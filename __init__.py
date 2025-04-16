7d238736aa317ebf75c3c3f3a704747fa77262d8
Drac <altdragon@outlook.de>
2025-04-16 21:13:07 +0200

    feat: add Passthrough node works the same as the switch nodes with fallback, just without the boolean switch. If input 2 is provided, input 2 will be outputted, otherwise it'll fall back/use input 1

--- __init__.py
+++ __init__.py
@@ -1,15 +1,18 @@
+from .modules.fallback_passthrough import FallbackPassthrough
 from .modules.fallback_switches import *
 
 NODE_CLASS_MAPPINGS = {
     "FallbackSwitchAny": FallbackSwitchAny,
     "FallbackSwitchImage": FallbackSwitchImage,
     "FallbackSwitchLatent": FallbackSwitchLatent,
+    "FallbackPassthrough": FallbackPassthrough,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "FallbackSwitchAny": "Switch Any Fallback 🐲",
     "FallbackSwitchImage": "Switch Image Fallback 🐲",
     "FallbackSwitchLatent": "Switch Latent Fallback 🐲",
+    "FallbackPassthrough": "Passthrough 🐲",
 }
 
 WEB_DIRECTORY = "./js"
---
+++ modules/fallback_passthrough.py
@@ -0,0 +1,47 @@
+from ..modules.utils.any_type import AnyType
+from ..modules.utils.ryuu_print import ryuu_print
+
+any_type = AnyType("*")
+
+
+class FallbackPassthrough:
+    """RyuuNoodles Fallback Passthrough Node.
+    This node has two inputs. If input_2_opt is provided, it will be used.
+    Otherwise input_1 will be used as fallback.
+    This node is similar to the Switch nodes except that there is no boolean input."""
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "input_1": (
+                    any_type,
+                    {
+                        "tooltip": "Will be used as fallback if input_2_opt is not provided.",
+                    },
+                ),
+            },
+            "optional": {
+                "input_2_opt": (
+                    any_type,
+                    {
+                        "tooltip": "Will be used if provided, otherwise input_1 will be used as fallback",
+                    },
+                )
+            },
+        }
+
+    RETURN_TYPES = (any_type,)
+    FUNCTION = "do_thing"
+    OUTPUT_NODE = True
+
+    DESCRIPTION = "This node allows for 2 inputs where input_2_opt is chosen if it's provided and otherwise input_1 is used as fallback."
+
+    CATEGORY = "RyuuNoodles/Util"
+
+    def do_thing(self, input_1, input_2_opt=None):
+        if input_2_opt is not None:
+            return (input_2_opt,)
+        else:
+            ryuu_print(f"No input_2_opt provided, falling back to input_1")
+            return (input_1,)

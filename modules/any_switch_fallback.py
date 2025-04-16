dd194a0609b46b2b247b5a24a6ebc5889c0529d0
Drac <altdragon@outlook.de>
2025-04-16 16:29:33 +0200

    refactor: change input names

--- modules/any_switch_fallback.py
+++ modules/any_switch_fallback.py
@@ -6,9 +6,9 @@ any_type = AnyType("*")
 class AnySwitchFallback:
     """RyuuNoodles Any Switch Fallback Node.
     This node takes a boolean input and two image inputs.
-    If the boolean is True, it returns image_1.
+    If the boolean is True, it returns input_1.
     If the boolean is False, it returns image2.
-    If image2 is not provided, it falls back to image_1 and a message will be printed to console.
+    If image2 is not provided, it falls back to input_1 and a message will be printed to console.
     """
 
     @classmethod
@@ -19,23 +19,23 @@ class AnySwitchFallback:
                     "BOOLEAN",
                     {
                         "default": False,
-                        "tooltip": "True will output image_1, False will try to output image_2_opt. "
-                        + "If image_2_opt is not provided (if for example the connected node is muted or bypassed), it will fall back to image_1. "
+                        "tooltip": "True will output input_1, False will try to output input_2_opt. "
+                        + "If input_2_opt is not provided (if for example the connected node is muted or bypassed), it will fall back to input_1. "
                         + "A message will be printed to console if this happens.",
                     },
                 ),
-                "image_1": (
+                "input_1": (
                     any_type,
                     {
-                        "tooltip": "The image to output if boolean is True. Will be used as fallback if image_2_opt is not provided",
+                        "tooltip": "The image to output if boolean is True. Will be used as fallback if input_2_opt is not provided",
                     },
                 ),
             },
             "optional": {
-                "image_2_opt": (
+                "input_2_opt": (
                     any_type,
                     {
-                        "tooltip": "The image to output if boolean is False. If not provided, image_1 will be used as fallback."
+                        "tooltip": "The image to output if boolean is False. If not provided, input_1 will be used as fallback."
                     },
                 ),
             },
@@ -47,12 +47,12 @@ class AnySwitchFallback:
 
     CATEGORY = "RyuuNoodles/Util"
 
-    def do_thing(self, boolean, image_1, image_2_opt=None):
+    def do_thing(self, boolean, input_1, input_2_opt=None):
         if boolean:
-            return (image_1,)
+            return (input_1,)
         else:
-            if image_2_opt is not None:
-                return (image_2_opt,)
+            if input_2_opt is not None:
+                return (input_2_opt,)
             else:
-                ryuu_print("No image_2_opt provided, falling back to image_1")
-                return (image_1,)
+                ryuu_print("No input_2_opt provided, falling back to input_1")
+                return (input_1,)

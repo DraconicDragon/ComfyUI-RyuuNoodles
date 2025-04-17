91e48356d512d6731a50634fce71ea4e13fef7f2
Drac <altdragon@outlook.de>
2025-04-17 19:01:15 +0200

    refactor: change switch input names to be more intuitive and remove redundant docstrings

--- modules/fallback_switches.py
+++ modules/fallback_switches.py
@@ -3,48 +3,24 @@ from ..modules.utils.any_type import AnyType
 
 
 class FallbackSwitchAny(FallbackSwitchBase):
-    """RyuuNoodles Any Switch Fallback Node.
-
-    This node has a boolean option and two inputs that can be any type.
-    If the boolean is True, it returns input_1.
-    If the boolean is False, it returns input_2_opt.
-    If input_2_opt is not provided, it falls back to input_1 and a message will be printed to console.
-    """
-
     TYPE = AnyType("*")
     RETURN_TYPES = (TYPE,)
-    INPUT_NAME_1 = "input_1"
-    INPUT_NAME_2 = "input_2_opt"
+    INPUT_NAME_1 = "on_true_fb"
+    INPUT_NAME_2 = "on_false"
 
 
 class FallbackSwitchImage(FallbackSwitchBase):
-    """RyuuNoodles Image Switch Fallback Node.
-
-    This node has a boolean option and two image inputs.
-    If the boolean is True, it returns image_1.
-    If the boolean is False, it returns image_2_opt.
-    If image_2_opt is not provided, it falls back to image_1 and a message will be printed to console.
-    """
-
     TYPE = "IMAGE"
     RETURN_TYPES = (TYPE,)
-    INPUT_NAME_1 = "image_1"
-    INPUT_NAME_2 = "image_2_opt"
+    INPUT_NAME_1 = "on_true_fb"
+    INPUT_NAME_2 = "on_false"
 
 
 class FallbackSwitchLatent(FallbackSwitchBase):
-    """RyuuNoodles Latent Switch Fallback Node.
-
-    This node has a boolean option and two latent inputs.
-    If the boolean is True, it returns latent_1.
-    If the boolean is False, it returns latent_2_opt.
-    If latent_2_opt is not provided, it falls back to latent_1 and a message will be printed to console.
-    """
-
     TYPE = "LATENT"
     RETURN_TYPES = (TYPE,)
-    INPUT_NAME_1 = "latent_1"
-    INPUT_NAME_2 = "latent_2_opt"
+    INPUT_NAME_1 = "on_true_fb"
+    INPUT_NAME_2 = "on_false"
 
 
 # class CustomSwitchFallback(FallbackSwitchBase):

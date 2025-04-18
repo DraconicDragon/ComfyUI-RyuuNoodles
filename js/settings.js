159451b30d5d05a981e859f783c47225b93878b7
Drac <altdragon@outlook.de>
2025-04-18 17:44:40 +0200

    feat: add settings.js for choosing nodes for token count overlay (no functionality currently)

---
+++ js/settings.js
@@ -0,0 +1,30 @@
+import { api } from "../../scripts/api.js";
+import { app } from "../../scripts/app.js";
+
+app.registerExtension({
+    name: "RyuuNoodles.Settings",
+    settings: [
+        {
+            id: "RyuuNoodles.TokenCountOverlay", // don't change, use category for name setting
+            // ["Category name", "Section heading", "Setting label"], but "Settings label" seems to be overwritten by 'name:'
+            category: ['RyuuNoodles 🐲', 'Token Count Overlay', 'Nodes'],
+            name: "Nodes configuration",
+            type: "text",
+            defaultValue: "Ryuu_TokenCountTextBox.input_text:CLIP_L,T5,T5_FAST;",
+            tooltip: "Enter the node names (internal nodeData.name), their widget names and tokenizer types. " +
+                "A token counter will appear above the supplied widget.\n" +
+                "Format: 'Node_Name.widget_name:Tokenizer1,Tok2,Tok3;Node_Name2.widget_name:Tok1;'.\n" +
+                "Example: 'Ryuu_TokenCountTextBox.input_text:CLIP_L,T5_FAST;'\n" +
+                "Currently accepted tokenizer types: CLIP_L, T5, T5_FAST (T5 is T5xxl) and LLAMA3.",
+            // todo: tooltips look ass, far too small (same goes for the textbox), annoying
+
+            onChange: async (newVal) => {
+                api.fetchApi("/ryuu/token_count_overlay/update", {
+                    method: "POST",
+                    body: JSON.stringify({ apikey: newVal }),
+                });
+            }
+        },
+    ],
+});
+

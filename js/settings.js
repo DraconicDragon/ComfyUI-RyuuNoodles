7cdef68042e019945a15f61fcd3d72aa7cfa560f
Drac <altdragon@outlook.de>
2025-04-18 19:20:51 +0200

    chore: comment out onChange API call, cleanup strings

--- js/settings.js
+++ js/settings.js
@@ -8,22 +8,22 @@ app.registerExtension({
             id: "RyuuNoodles.TokenCountOverlay", // don't change, use category for name setting
             // ["Category name", "Section heading", "Setting label"], but "Settings label" seems to be overwritten by 'name:'
             category: ['RyuuNoodles 🐲', 'Token Count Overlay', 'Nodes'],
-            name: "Nodes configuration",
+            name: "Nodes configuration (Reload required on change)",
             type: "text",
-            defaultValue: "Ryuu_TokenCountTextBox.input_text:CLIP_L,T5,T5_FAST;",
+            defaultValue: "Ryuu_TokenCountTextBox.input_text:CLIP_L,T5_FAST;",
             tooltip: "Enter the node names (internal nodeData.name), their widget names and tokenizer types. " +
                 "A token counter will appear above the supplied widget.\n" +
                 "Format: 'Node_Name.widget_name:Tokenizer1,Tok2,Tok3;Node_Name2.widget_name:Tok1;'.\n" +
                 "Example: 'Ryuu_TokenCountTextBox.input_text:CLIP_L,T5_FAST;'\n" +
-                "Currently accepted tokenizer types: CLIP_L, T5, T5_FAST (T5 is T5xxl) and LLAMA3.",
+                "Currently accepted tokenizer types: CLIP_L, T5, T5_FAST and LLAMA3.",
             // todo: tooltips and textbox are far too small for this :pensive:
 
-            onChange: async (newVal) => {
-                api.fetchApi("/ryuu/token_count_overlay/update", {
-                    method: "POST",
-                    body: JSON.stringify({ apikey: newVal }),
-                });
-            }
+            // onChange: async (newVal) => {
+            //     api.fetchApi("/ryuu/token_count_overlay/update", {
+            //         method: "POST",
+            //         body: JSON.stringify({ apikey: newVal }),
+            //     });
+            // }
         },
     ],
 });

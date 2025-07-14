4349ef0b348656d3adbbe55088df28acbceb294b
draconicdragon <altdragon@outlook.de>
2025-07-14 20:20:59 +0200

    chore: add debug log

--- pyserver/loglevel.py
+++ pyserver/loglevel.py
@@ -2,6 +2,7 @@ import os
 
 from aiohttp import web
 
+from ..modules.shared.ryuu_log import ryuu_log
 from server import PromptServer  # type: ignore
 
 routes = PromptServer.instance.routes
@@ -17,7 +18,7 @@ async def set_loglevel(request):
     loglevel = data.get("loglevel", "")
     os.environ["RYUU_LOGLEVEL"] = loglevel.upper()
 
-    # ryuu_log("Log level set to:", loglevel.upper(), loglevel=loglevel.lower())
+    ryuu_log("Log level set to:", loglevel.upper(), loglevel="debug")
     # ryuu_log("DEBUG Log Level test", loglevel="debug")
     # ryuu_log("INFO Log Level test", loglevel="info")
     # ryuu_log("WARNING Log Level test", loglevel="warning")
